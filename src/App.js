a999f403b9eec97dad90e960926a2c822313683d
Backsta3 <darkstarfar35@gmail.com>
2020-11-24 03:36:11 +0600

    Apartment Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import "./App.css";
-import Registration from "./components/Registration/Registration";
+import React, { createContext, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import ApartmentMain from './components/ApartmentMain/ApartmentMain';
+
+
+
+
+
+export const UserContext = createContext();
 
 function App() {
-  return <div>
-    <Registration></Registration>
-  </div>;
+  const [admin, setAdmin] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState({});
+  useEffect(() => {
+    fetch(`${loggedInUser.email}`)
+      .then(res => res.json())
+      .then(data => {
+        setAdmin(data);
+      })
+  }, [loggedInUser.email]);
+
+
+  console.log(loggedInUser);
+  return (
+    <div className="App">
+      <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+        <Router>
+          <div>
+            <Switch>
+             <Route >
+              <ApartmentMain></ApartmentMain>
+              </Route>
+             </Switch>
+          </div>
+        </Router>
+      </UserContext.Provider>
+    </div>
+  );
 }
+
 export default App;
