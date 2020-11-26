7923b0813e5eba3fc4597b2d3d76dbf356a0b6d3
InjamulRashik <ihrashik@gmail.com>
2020-11-27 00:01:40 +0600

    Master Checking!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import ApartmentMain from './components/ApartmentMain/ApartmentMain';
-import Appartments from './components/Appartments/Appartments';
-import Dashboard from './components/Dashboard/Dashboard/Dashboard';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Registration from './components/Registration/Registration';
-
-
-
-
-
+import React, { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import ApartmentMain from "./components/ApartmentMain/ApartmentMain";
+import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 
 export const UserContext = createContext();
 
 function App() {
-
   const [admin, setAdmin] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState({});
   useEffect(() => {
     fetch(`${loggedInUser.email}`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         setAdmin(data);
-      })
+      });
   }, [loggedInUser.email]);
-
   console.log(loggedInUser);
   return (
     <div className="App">
@@ -34,23 +26,17 @@ function App() {
         <Router>
           <div>
             <Switch>
-            <Route exact path="/">
-                <Home></Home>
-              </Route>
-
+              <Route exact path="/">
                 <Home></Home>
               </Route>
               <Route path="/login">
                 <Login></Login>
               </Route>
-              <Route path="/reg">
-                <Registration></Registration>
-              </Route>
               <Route path="/ApartmentMain/:_id">
-              <ApartmentMain></ApartmentMain>
+                <ApartmentMain></ApartmentMain>
               </Route>
               <Route path="/dashboard">
-              <Dashboard></Dashboard>
+                <Dashboard></Dashboard>
               </Route>
               <Route path="/booking">
                 <ApartmentMain></ApartmentMain>
